51464c5b88e0ebb06067f1936277045ab2ce67b3
Paulina Valdés Campos <paulina.valdes@arauco.cl>
2019-06-17 12:39:46 -0400

    add dashboard

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('active');
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+	
+	$('input').focus(function(){
+	  $(this).parents('.form-group').addClass('focused');
+	});
+	
+	$('input').blur(function(){
+	  var inputValue = $(this).val();
+	  if ( inputValue == "" ) {
+	    $(this).removeClass('filled');
+	    $(this).parents('.form-group').removeClass('focused');  
+	  } else {
+	    $(this).addClass('filled');
+	  }
+	})  
+	
+	var table = $('#example').DataTable({
+        'responsive': true
+    });
+
+    // Handle click on "Expand All" button
+    $('#btn-show-all-children').on('click', function(){
+        // Expand row details
+        table.rows(':not(.parent)').nodes().to$().find('td:first-child').trigger('click');
+    });
+
+    // Handle click on "Collapse All" button
+    $('#btn-hide-all-children').on('click', function(){
+        // Collapse row details
+        table.rows('.parent').nodes().to$().find('td:first-child').trigger('click');
+    });
+	
+	
+	
+});
